f8c8612c99056ac75198735b3c0a4d09adc618ee
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-10-09 19:14:17 -0300

    Model veiculo criada

---
+++ ConsumoVeiculo/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsumoVeiculo.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informe o nome do veículo.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informe a placa do veículo.")]
+        public string Placa { get; set; }
+
+        [Required(ErrorMessage = "Informe o ano de fabricação do veículo.")]
+        public int AnoFabricacao { get; set; }
+
+        [Required(ErrorMessage = "Informe o ano do modelo do veículo.")]
+        public int AnoModelo { get; set; }
+    }
+}
