f611456b0e1c956cdd03037792558fd8df51cc84
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-10-11 19:42:19 -0300

    Imprementação do EDIT de veículos

--- ConsumoVeiculo/Controllers/VeiculoController.cs
+++ ConsumoVeiculo/Controllers/VeiculoController.cs
@@ -36,5 +36,39 @@ namespace ConsumoVeiculo.Controllers
             }
             return View(veiculo);
         }
+
+        public async Task<IActionResult> Edit(int? id) 
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var dados = await _context.Veiculos.FindAsync(id);
+
+            if (dados == null)
+            {
+                return NotFound();
+            }
+
+            return View(dados);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Veiculo veiculo)
+        {
+            if (id != veiculo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Veiculos.Update(veiculo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
