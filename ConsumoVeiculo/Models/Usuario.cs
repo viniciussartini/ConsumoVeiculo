56112c98881cd3b87a8f854c85f459fe2fc000ba
Vinicius Sartini <sartini.vinicius@outlook.com>
2024-10-12 13:55:06 -0300

    Implementação do CRUD e Views Usuario

---
+++ ConsumoVeiculo/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ConsumoVeiculo.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Informar o nome.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informar a senha.")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Informar o perfil de usuário.")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil
+    {
+        Admin,
+        User
+    }
+}
